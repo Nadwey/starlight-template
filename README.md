a97325116f88ff44ead09728ea62a53c2943b5e6
Nadwey <nadweyyy@gmail.com>
2025-08-05 22:44:28 +0200

    Update template

--- README.md
+++
@@ -1,5 +0,0 @@
-# Starlight Template
-
-From the ashes of [NadArenas](https://github.com/Nadwey/NadArenas), a [Starlight](https://starlight.astro.build/) template is born.
-
-Because I hate the default theme.
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,52 @@
+// @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    base: "/starlight-template/",
+    site: "https://starlight-template.nadwey.pl",
+    vite: {
+        // @ts-expect-error: https://github.com/withastro/astro/issues/14030
+        plugins: [tailwindcss()],
+    },
     integrations: [
         starlight({
             title: "Starlight Template",
-            social: {
-                github: "https://github.com/Nadwey/starlight-template",
+            components: {
+                PageFrame: "./src/components/overrides/PageFrame.astro",
+                PageTitle: "./src/components/overrides/PageTitle.astro",
+                Pagination: "./src/components/overrides/Pagination.astro",
+                Sidebar: "./src/components/overrides/Sidebar.astro",
+                TwoColumnContent: "./src/components/overrides/TwoColumnContent.astro",
             },
-            customCss: [
-                "./src/styles/custom.css",
-                "./src/fonts/font-face.css",
+            customCss: ["./src/styles/fonts.css", "./src/styles/globals.css", "./src/styles/docs.css"],
+            logo: { src: "./src/assets/img/favicon.png", alt: "Starlight Template" },
+            favicon: "./favicon.png",
+            social: [
+                { icon: "github", label: "GitHub", href: "https://github.com/Nadwey/starlight-template" },
             ],
             sidebar: [
                 {
-                    label: "👋 Welcome",
-                    link: "/",
-                },
-                {
-                    label: "📄 Dummy Page",
-                    link: "/dummy",
+                    label: "Welcome",
+                    slug: "docs/welcome",
                 },
             ],
             expressiveCode: {
-                plugins: [pluginLineNumbers()]
-            }
+                themes: ["slack-dark", "light-plus"],
+                plugins: [pluginLineNumbers()],
+                defaultProps: {
+                    showLineNumbers: false,
+                    overridesByLang: {
+                        yaml: {
+                            showLineNumbers: true,
+                        },
+                    },
+                },
+            },
         }),
+        react(),
     ],
 });
